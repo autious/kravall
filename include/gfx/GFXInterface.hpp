acc4f58deff6fe161fd9631d9e2b3cf243d8517a
Joel Svensson <joeliboy@hotmail.com>
2013-11-26 10:53:03 +0100

    Links core and gfx lib

--- include/gfx/GFXInterface.hpp
+++ include/gfx/GFXInterface.hpp
@@ -1,12 +1,36 @@
-#ifdef _WIN32
-#ifdef DLL_EXPORT
+#ifdef GFX_DLL_EXPORT
 #define DLL_API __declspec(dllexport)
 #else
 #define DLL_API __declspec(dllimport)
 #endif
-#endif
+
+#include <GLFW/glfw3.h>
+
+#define GFX_SUCCESS 0
+#define GFX_FAIL 1
 
 namespace GFX
 {
-	static DLL_API void Render();
+	/*!
+	Binds a context to a window and initializes the graphics engine.
+	\param window "window" is the window you wish to bind the context to
+	\return Returns GFX_SUCCESS if successful, else returns GFX_FAIL
+	*/
+	DLL_API int Init(GLFWwindow* window);
+
+	/*!
+	Executes all draw calls made to the graphics engine and
+	renders it to the screen.
+	*/
+	DLL_API void Render(GLFWwindow* window);
+}
+
+namespace GFXDebug
+{
+
+}
+
+namespace GFXData
+{
+
 }
