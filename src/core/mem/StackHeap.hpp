7913cb5c89d106407564c1c9210ed4dc7e8fd3f0
alexander vestman <alexander.vestman@gmail.com>
2013-11-28 15:07:00 +0100

    Adds functions to the StackHeap class

--- src/core/mem/StackHeap.hpp
+++ src/core/mem/StackHeap.hpp
@@ -7,12 +7,29 @@ struct Finalizer;
 
 namespace Core
 {
+    /*!
+      Stack based memory manager, all objects that are allocated with the StackHeap are removed once the StackHeap goes out of scope. ObjectsAllocated with AllocateObject have their destructor called on removal.
+      */
     class StackHeap
     {
     public:
-       explicit  StackHeap();
+       /*!
+         Constructor for StackHeap.
+         \param allocator The linear allocator that is used to allocate memory.
+         */
+       explicit StackHeap(Core::LinearAllocator& allocator);
+
+       /*!
+         Allocates a new Plain Old Data structure, once the StackHeap goes out of scope a destructor will not be called on this allocation.
+         */
+       template<typename T> T* NewPOD();
+       /*!
+         Allocates a new object, once the StackHeap goes out of scope a destructor will be called on the new object. 
+         */
+       template<typename T> T* NewObject();
 
     private:
+       Finalizer* AllocateObjectInternal
        Finalizer* m_finalizerChain;
     };
 }
