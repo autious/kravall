f6e09e9305847b9f34755a7c1569077c3c97e6f8
Max Danielsson <maxdata1@gmail.com>
2013-11-28 08:48:58 +0100

    T

---
+++ src/core/ComponentFramework/EntityHandler.cpp
@@ -0,0 +1,1 @@
+#include "EntityHandler.hpp"
--- src/core/ComponentFramework/EntityHandler.hpp
+++ src/core/ComponentFramework/EntityHandler.hpp
@@ -1,13 +1,35 @@
 #ifndef SRC_CORE_COMPONENTFRAMEWORK_ENTITYHANDLER_H
 #define SRC_CORE_COMPONENTFRAMEWORK_ENTITYHANDLER_H
 
+#include <typeinfo>
+
+typedef unsigned int Entity;
+
 namespace Core
 {
+    template<typename... Components>
     class EntityHandler
     {
-        struct ComponentMap
+    private:
+        class ComponentMap
         {
-        };   
+            constexpr ComponentMap()
+            {
+                
+            }
+
+            void constexpr GetId()
+            {
+            
+            }
+
+            static constexpr std::type_info m_infoId[sizeof...(Components)];
+        };
+    public:
+        EntityHandler();
+
+        void *m_components[sizeof...(Components)];
+        int m_componentSizes[sizeof...(Components)];
     };
 }
 
