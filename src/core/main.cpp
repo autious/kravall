7212ebea177a8424bb2c4a89d4b01ecc46f0cd6c
Tim Henriksson <henriksson.tim@gmail.com>
2013-11-29 14:19:00 +0100

    Important color changes

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -51,7 +51,7 @@ void run( GLFWwindow * window )
 		GFX::Debug::DrawBox(glm::vec3(2, 0, 0), glm::vec3(2, 2, 10), true, glm::vec4(Colors::Black.x, Colors::Black.y, Colors::Black.z, 0.5f));
 		GFX::Debug::DrawSphere(glm::vec3(2, 0, 0), 2.0f, Colors::Black);
 		
-		GFX::Debug::DrawLine(glm::vec2(100, 50), glm::vec2(1200, 600), Colors::Red);
+		GFX::Debug::DrawLine(glm::vec2(100, 50), glm::vec2(1200, 600), Colors::CornflowerBlue);
 		GFX::Debug::DrawPoint(glm::vec2(100, 50), Colors::Green, 10);
 		GFX::Debug::DrawPoint(glm::vec2(1200, 600), Colors::Green, 10);
 		
@@ -61,7 +61,7 @@ void run( GLFWwindow * window )
 		GFX::RenderText(glm::vec2(0, 100), glm::vec2(8, 12), Colors::Black, "The Quick Brown Fox Jumps Over The Lazy Dog");
 		GFX::RenderText(glm::vec2(10, 120), glm::vec2(7, 11), Colors::Blue, "The Quick Brown Fox Jumps Over The Lazy Dog");
 		GFX::RenderText(glm::vec2(20, 140), glm::vec2(6, 10), Colors::Green, "The Quick Brown Fox Jumps Over The Lazy Dog");
-		GFX::RenderText(glm::vec2(30, 160), glm::vec2(5, 9), Colors::Red, "The Quick Brown Fox Jumps Over The Lazy Dog");
+		GFX::RenderText(glm::vec2(30, 160), glm::vec2(5, 9), Colors::CornflowerBlue, "The Quick Brown Fox Jumps Over The Lazy Dog");
 		GFX::RenderText(glm::vec2(40, 180), glm::vec2(8, 12), Colors::White, "The Quick Brown Fox Jumps Over The Lazy Dog????");
 
 		GFX::RenderText(glm::vec2(0, 200), glm::vec2(8, 12), Colors::Gold, "ABCDEFGHIJKLMNOPQRSTUVWXYZASIUHDOIASHUDIOASHDA1234567890*'^&%#!?");
