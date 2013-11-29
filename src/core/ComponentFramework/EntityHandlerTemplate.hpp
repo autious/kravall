923fcbac63987453f34335e9eee7b6c7d0fdd6c9
Max Danielsson <maxdata1@gmail.com>
2013-11-29 12:32:28 +0100

    Moves match template hack to separate header.

--- src/core/ComponentFramework/EntityHandlerTemplate.hpp
+++ src/core/ComponentFramework/EntityHandlerTemplate.hpp
@@ -4,6 +4,7 @@
 #include "PVector.hpp"
 #include "EntityVector.hpp"
 #include "ComponentType.hpp"
+#include "CheckTemplatePresence.hpp"
 
 #include <cstdint>
 #include <cassert>
@@ -17,42 +18,6 @@ namespace Core
     template<typename... Components>
     class EntityHandlerTemplate
     {
-
-        //**********************************************************************
-        
-        /*!
-            This template hack is for checking if a component
-            given in an function exists for the handler.
-        */
-        template <class... R>
-        struct Match;
-
-        template <class T, class U>
-        struct Match<T,U>
-        {
-            static const bool exists = false;
-        };
-
-        template <class T>
-        struct Match<T,T>
-        {
-            static const bool exists = true;
-        };
-
-        template <class T, class... R> 
-        struct Match<T,T,R...>
-        {
-            static const bool exists = true;
-        };
-
-        template <class T, class U, class... R> 
-        struct Match<T,U,R...>
-        {
-            static const bool exists = Match<T,R...>::exists;
-        };
-
-        //**********************************************************************
-        
         static const int COMPONENT_COUNT = sizeof...(Components);
 
         std::vector<int[COMPONENT_COUNT]> m_entityIds;
