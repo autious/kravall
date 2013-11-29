4b5ac2f58e9fe2ff31f9590c3f7ad7eda7655753
Tim Henriksson <henriksson.tim@gmail.com>
2013-11-29 14:58:10 +0100

    TextPainter assert

--- src/gfx/Renderer/TextRenderer/TextPainter.cpp
+++ src/gfx/Renderer/TextRenderer/TextPainter.cpp
@@ -22,9 +22,9 @@ namespace GFX
 		}
 
 		/* Load a font */
-		if (FT_New_Face(m_library, "assets/Fonts/DentonBETA2.ttf", 0, &m_face))
+		if (FT_New_Face(m_library, "assets/Fonts/DentonBETA22.ttf", 0, &m_face))
 		{
-			std::cout << "Could not open font\n";
+			assert(0 && "Could not open font file in TextPainter.Initialize()\n");
 		}
 
 		m_shaderManager->CreateProgram("BasicText");
