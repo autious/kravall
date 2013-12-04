6a88491a0982e1db0fe520d1ef46d9cca0fbbf1c
Max Danielsson <maxdata1@gmail.com>
2013-12-04 15:08:58 +0100

    Attempt at mainpage.

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,3 +1,15 @@
+/*! \mainpage Our Index Page
+
+    \section intro_sec Introduction
+
+    This is the introduction.
+
+    \section install_sec Installation
+
+    \subsection step1 Step 1: Opening the box
+
+*/
+
 #ifdef RUN_GTEST 
     #include <gtest/gtest.h>
 #endif
