fc21940048a22593ddc64fe34f250d5dec3a7906
alexander vestman <alexander.vestman@gmail.com>
2013-12-16 12:37:55 +0100

    Adds doxygen comments to TTFLoader.hpp

--- src/core/ContentManagement/Loaders/TTFLoader.hpp
+++ src/core/ContentManagement/Loaders/TTFLoader.hpp
@@ -19,12 +19,40 @@ namespace Core
     class TTFLoader: public Core::BaseAssetLoader
     {
     public:
+        /*!
+          Constructor for TTFLoader, creates the FreeType library.
+          */
         TTFLoader();
+
+        /*!
+          Destructor for TTFLoader, destroyes all loaded fonts and faces.
+          */
         ~TTFLoader();
 
+        /*!
+          Synchronous load of fonts.
+          \param assetName the name of the asset meta file of the font.
+          \returns An AssetHandle that is complete and ready for use.
+          */
         Core::AssetHandle Load(const char* assetName) override;
+
+        /*!
+          Asynchronous load of fonts.
+          \param assetName the name of the asset meta file of the font.
+          \returns An AssetHandle that is not yet ready for use.
+          */
         Core::AssetHandle LoadAsync(const char* assetName) override;
+
+        /*!
+          Finishing function that applies the asynchronous loaded data to state dependent memory.
+          \param handle An in-out going parameter that is used to pass data in and out of the function. The handle is ready to use after call.
+          */
         void FinishLoadAsync(Core::AssetHandle& handle) override;
+
+        /*!
+          Destroys the given font, removing it from memory. The handle can no longer be used after calling this function.
+          \param handle The asset handle to destroy.
+          */
         void Destroy(const Core::AssetHandle handle) override;
 
     private:
