2ecb4fe1d52bf6cf24843cd7f963e4a0a22ccc09
alexander vestman <alexander.vestman@gmail.com>
2013-12-19 11:15:24 +0100

    Adds Event support and state saving

--- src/core/Input/InputManager.cpp
+++ src/core/Input/InputManager.cpp
@@ -1,44 +1,66 @@
 #include "InputManager.hpp"
 
+#include <World.hpp>
 
 namespace Core
 {
-    void InputManagerKeyCallback(GLFWwindow* window, intkey, int scancode, int action, int mods)
+    void InputManagerKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
     {
+        Core::KeyboardState kb_State = InputManager::GetKeyboardState():
+        kb_State.SetKeyState(key, action);
 
+        KeyEvents[nKeyEvents].key = key;
+        KeyEvents[nKeyEvents].action= action;
+        KeyEvents[nKeyEvents].mods = mods;
+        nKeyEvents++;
     }
 
     void InputManagerCharCallback(GLFWwindow* window, unsigned int codepoint)
     {
-
+        CharEvents[nCharEvents].codepoint = codepoint;
+        nCharEvents++;
     }
 
     void InputManagerMousebuttonCallback(GLFWwindow* window, int button, int action, int mods)
     {
+        Core::MouseState mouseState = InputManager::GetMouseState();
+        mouseState.SetButtonState(button, action);
 
+        MouseEvents[nMouseEvents].button = button;
+        MouseEvents[nMouseEvents].action = action;
+        MouseEvents[nMouseEvents].mods = mods;
+        nMouseEvents++;
     }
 
     void InputManagerCursorPositionCallback(GLFWwindow* window, double xpos, double ypos)
     {
-
+        PositionEvents[nPositionEvents].xpos = xpos;
+        PositionEvents[nPositionEvents].ypos = ypos;
+        nPositionEvents++;
     }
 
     void InputManagerMouseScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
     {
-
+        ScrollEvents[nScrollEvents].xoffset = xoffset;
+        ScrollEvents[nScrollEvents].yoffset = yoffset;
+        nScrollEvents++;
     }
 
     InputManager::InputManager(GLFWwindow* window)
     {
         m_window = window;
-        m_keyboardState = Core::KeyboardState();
-        m_mouseState = Core::MouseState();
 
         glfwSetKeyCallback(m_window, InputManagerKeyCallback);
         glfwSetCharCallback(m_window, InputManagerCharCallback);
         glfwSetMouseButtonCallback(m_window, InputManagerMouseButtonCallback);
         glfwSetCursorPosCallback(m_window, InputManagerCursorPositionCallback);
         glfwSetScrollCallback(m_window, InputManagerMouseScrollCallback);
+
+        KeyEvents = Core::world.m_constantHeap.NewPODArray<Core::KeyEvent>(NUMBER_OF_KEY_EVENTS); 
+        CharEvents = Core::world.m_constantHeap.NewPODArray<Core::CharEvent>(NUMBER_OF_CHAR_EVENTS); 
+        MouseEvents = Core::world.m_constantHeap.NewPODArray<Core::MouseEvent>(NUMBER_OF_MOUSE_EVENTS); 
+        PositionEvents = Core::world.m_constantHeap.NewPODArray<Core::PositionEvent>(NUMBER_OF_POSITION_EVENTS); 
+        ScrollEvents = Core::world.m_constantHeap.NewPODArray<Core::ScrollEvent>(NUMBER_OF_SCROLL_EVENTS); 
     }
 
     InputManager::~InputManager()
@@ -48,7 +70,14 @@ namespace Core
 
     void InputManager::PollEvents()
     {
+        nKeyEvents = 0;
+        nCharEvents = 0;
+        nMouseEvents = 0;
+        nPositionEvents = 0;
+        nScrollEvents = 0;
+
         glfwPollEvents();
+
         int x = 0;
         int y = 0; 
         glfwGetCursorPos(window, x, y);
@@ -58,15 +87,5 @@ namespace Core
     void InputManager::CallListeners()
     {
 
-    }
-
-    const Core::KeyboardState& InputManager::GetKeyboardState() const
-    {
-        return m_keyboardState;
-    }
-
-    const Core::MouseState& InputManager::GetMouseState()const
-    {
-        return m_mouseState;
     }
 }
--- src/core/Input/InputManager.hpp
+++ src/core/Input/InputManager.hpp
@@ -12,14 +12,26 @@
 #include <Input/Events/PositionEvent.hpp>
 #include <Input/Events/ScrollEvent.hpp>
 
+
 namespace Core
 {
+    namespace 
+    {   
+        Core::KeyboardState keyboardState();
+        Core::MouseState mouseState();
+    }
 
     Core::KeyEvent* KeyEvents;
     Core::CharEvent* CharEvents;
     Core::MouseEvent* MouseEvents;
     Core::PositionEvent* PositionEvents;
     Core::ScrollEvent* ScrollEvents;
+
+    int nKeyEvents = 0;
+    int nCharEvents = 0;
+    int nMouseEvents = 0;
+    int nPositionEvents = 0;
+    int nScrollEvents = 0;
 
     class InputManager
     {
@@ -29,31 +41,23 @@ namespace Core
 
         void PollEvents();
         void CallListeners();
-        const Core::KeyboardState GetKeyboardState();
-        const Core::MouseState GetMouseState();
+        inline static Core::KeyboardState& GetKeyboardState(){ return keyboardState; }
+        inline static Core::MouseState& GetMouseState(){ return mouseState; }
 
     private:
-        static const unsigned int NUMBER_OF_KEY_EVENTS = 100;
-        static const unsigned int NUMBER_OF_CHAR_EVENTS = 100;
-        static const unsigned int NUMBER_OF_MOUSE_EVENTS = 50;
-        static const unsigned int NUMBER_OF_POSITION_EVENTS = 300;
-        static const unsigned int NUMBER_OF_SCROLL_EVENTS = 50;
+        static const int NUMBER_OF_KEY_EVENTS = 300;
+        static const int NUMBER_OF_CHAR_EVENTS = 300;
+        static const int NUMBER_OF_MOUSE_EVENTS = 50;
+        static const int NUMBER_OF_POSITION_EVENTS = 300;
+        static const int NUMBER_OF_SCROLL_EVENTS = 50;
 
         GLFWwindow* m_window;
-        Core::KeyboardState m_keyboardState;
-        Core::MouseState m_mouseState;
         
         Core::KeyEvent* m_startKeyEventPtr;
         Core::CharEvent* m_startCharEventPtr;
         Core::MouseEvent* m_startMouseEventPtr;
         Core::PositionEvent* m_startPositionEventPtr;
         Core::ScrollEvent* m_startScrollEventPtr;
-
-        Core::KeyEvent* m_currentKeyEventPtr;
-        Core::CharEvent* m_currentCharEventPtr;
-        Core::MouseEvent* m_currentMouseEventPtr;
-        Core::PositionEvent* m_currentPositionEventPtr;
-        Core::ScrollEvent* m_currentScrollEventPtr;
     };
 }
 
