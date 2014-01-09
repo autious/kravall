c316ba3822f26d98b97b051fd95daa28b0b11ea0
John Heiding <John_Heiding@hotmail.com>
2014-01-09 09:25:44 +0100

    small changes, mostly lua

--- scripts/scenarios/testtest.lua
+++ scripts/scenarios/testtest.lua
@@ -31,17 +31,20 @@ local rioter = require "entities/teapot"
 local police = require "entities/police"
 					
 for i = -1, 3 do
-	for p = 0, 10 do
-		rioter.create( asm, p * 5, 0, i * 2 )
+	for p = 1, 20 do
+		--rioter.create( asm, -10 + p * 2, 0, 5 + i * 2 )
 	end
 end
 
-for i = 0, 1 do	
-	for p = 0, 10 do
-		police.create( asm, -5 + i * 1.6, 0, p * 1.6 )
-	end
-end
---collectgarbage()
+rioter.create( asm, -10, 0, 5, 1, 0 )
+rioter.create( asm, 10, 0, 5, -1, 0 )
+
+--for i = 0, 1 do	
+--	for p = 1, 10 do
+--		police.create( asm, -15 + i * 1.6, 0, p * 1.6 )
+--	end
+--end
+--collectgarbage()	
 return asm;
 
 
