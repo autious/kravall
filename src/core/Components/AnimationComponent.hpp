303675ec22570edcd769b329e714778b993f3302
Tim Henriksson <henriksson.tim@gmail.com>
2014-01-20 16:04:13 +0100

    animation component updated

--- src/core/Components/AnimationComponent.hpp
+++ src/core/Components/AnimationComponent.hpp
@@ -9,7 +9,6 @@ namespace Core
 	{
 		int skeletonID;
 		int animationID;
-		float animationLengthSeconds;
 		float currentTime;
 		bool loop;
 		bool playing;
@@ -18,25 +17,9 @@ namespace Core
 		{
 			skeletonID	= -1;
 			animationID = -1;
-			animationLengthSeconds = 0.0f;
 			currentTime = 0.0f;
 			loop = false;
 			playing = false;
-		}
-
-		inline static void PlayAnimation(int animationID, AnimationComponent& ac)
-		{
-			ac.animationID = animationID;
-			ac.currentTime = 0.0f;
-			ac.playing = true;
-		}
-
-		inline static void LoopAnimation(int animationID, AnimationComponent& ac)
-		{
-			ac.animationID = animationID;
-			ac.currentTime = 0.0f;
-			ac.playing = true;
-			ac.loop = true;
 		}
 
 		inline static const char * GetName()
