2d07dc3e522afc242a5028c0ef5bfe2d09d5216e
Max Danielsson <maxdata1@gmail.com>
2014-01-20 15:23:50 +0100

    Adds support for multiple bags in systems.

---
+++ src/core/GameUtility/AreaUtility.hpp
@@ -0,0 +1,28 @@
+namespace Core
+{
+    namespace AreaUtility
+    {
+        inline bool CheckPointInsideNode( glm::vec3 point, float * points )
+        {
+            for( int i = 0; i < 4; i++ )
+            {
+                int ii = i * 2;
+                int oo = (ii + 2) % 8;
+
+                glm::vec3 lineStart = glm::vec3( points[ ii ], 0.0f, points[ ii + 1 ] );
+                glm::vec3 lineEnd	= glm::vec3( points[ oo ], 0.0f, points[ oo + 1 ] );
+
+                // calc normal
+                glm::vec3 cross = glm::normalize( glm::cross( (lineEnd - lineStart), glm::vec3( 0.0f, 1.0f, 0.0f ) ) );
+
+                // check if behind plane
+                float dot = glm::dot( cross, point - lineStart );
+                if( dot <= 0 )
+                    return false;
+            }
+
+            // point is inside all lines
+            return true;
+        }
+    }
+}
