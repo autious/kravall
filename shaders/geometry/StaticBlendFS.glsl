519d42689c0cb789559653b4ad279e1ddb9efd17
Joel Svensson <joeliboy@hotmail.com>
2014-01-21 14:56:24 +0100

    Adds color variation in blend shader

--- shaders/geometry/StaticBlendFS.glsl
+++ shaders/geometry/StaticBlendFS.glsl
@@ -5,7 +5,7 @@ in vec4 posW;
 in vec4 normalFS;
 in vec4 tangentFS;
 in vec2 uvFS;
-//in uint rnd_seed;
+flat in uint rnd_seed;
 
 uniform sampler2D diffuseMap;
 uniform sampler2D normalMap; // Used here as tone map
@@ -18,25 +18,44 @@ layout ( location = 3 ) out vec4 specularRT;
 layout ( location = 4 ) out vec4 glowMatIDRT;
 
 
+vec4 randColor( uint seed )
+{
+	float tr = (seed%16)/16.0f;
+	float tg = (seed%8)/8.0f;
+	float tb = (seed%4)/4.0f;
+	return vec4( 
+					0.3 + 0.4 * tr, 
+					0.3 + 0.4 * tg, 
+					0.3 + 0.4 * tb, 
+					1.0f 
+				);
+}
 
 void main()
 {
-	vec4 diffuseColor = texture2D(diffuseMap, uvFS);
+	vec4 diffuseColor = vec4(1);//texture2D(diffuseMap, uvFS);
 	vec4 blendMap = texture2D(normalMap, uvFS);
 	vec4 specColor = texture2D(specularMap, uvFS);
 	vec4 glowColor = texture2D(glowMap, uvFS);
 
-	// Blend colors
-	vec4 colorR = vec4( 0.18, 0.13, 0.12, 1.0f ); // Dark skin tone
-	vec4 colorG = vec4( 1.0f, 0.0f, 0.0f, 1.0f ); // Shirt
-	vec4 colorB = vec4( 0.09, 0.39, 0.88, 1.0f ); // Pants
-	//vec4 colorA = vec4( 1.0f, 0.0f, 1.0f, 1.0f ); // Extra
+	// interpolate skin color from light to dark
+	float factor = (rnd_seed%16)/16.0f;
+	vec4 colorR = mix(vec4( 1.0f, 0.89, 0.78, 1.0f ), vec4( 0.18, 0.13, 0.12, 1.0f ), factor);  // Dark skin tone
+	
+	// Create random colors for shirts
+	vec4 colorG = randColor(rnd_seed); // Shirt
+
+	// Create random colors for pants
+	vec4 colorB = randColor(rnd_seed + 55);// 0.5 * (vec4(0.1f, 0.5f, 0.8f, 1.0f) + randColor(rnd_seed+52)); // Pants
+	
+	// Create random colors for Accessories/Extra
+	//vec4 colorA = randColor(rnd_seed + 55); // Extra
 	
 	vec4 result = diffuseColor;
-	result = mix(result, diffuseColor*colorR, blendMap.r);
-	result = mix(result, diffuseColor*colorG, blendMap.g);
-	result = mix(result, diffuseColor*colorB, blendMap.b);
-	//diffuseColor = mix(diffuseColor, colorA, blendMap.a);
+	result = mix(result, diffuseColor*colorR, blendMap.r); // Skin
+	result = mix(result, diffuseColor*colorG, blendMap.g); // Shirt
+	result = mix(result, diffuseColor*colorB, blendMap.b); // Pants
+	//result = mix(result, diffuseColor*colorA, blendMap.a); // Extra
 
 	// Save to render targets
 	normalDepthRT = vec4(normalize(normalFS.xyz), posFS.z / posFS.w);
