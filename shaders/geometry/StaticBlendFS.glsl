39ca3ee5700e1b0e20aa17551756d11e1a5af431
Joel Svensson <joeliboy@hotmail.com>
2014-01-21 18:49:57 +0100

    Add random function for rioter colors

--- shaders/geometry/StaticBlendFS.glsl
+++ shaders/geometry/StaticBlendFS.glsl
@@ -17,12 +17,31 @@ layout ( location = 2 ) out vec4 diffuseRT;
 layout ( location = 3 ) out vec4 specularRT;
 layout ( location = 4 ) out vec4 glowMatIDRT;
 
+uint wang_hash(uint seed)
+{
+    seed = (seed ^ 61) ^ (seed >> 16);
+    seed *= 9;
+    seed = seed ^ (seed >> 4);
+    seed *= 0x27d4eb2d;
+    seed = seed ^ (seed >> 15);
+    return seed;
+}
+
+// Xorshift algorithm from George Marsaglia's paper
+float rand( uint seed )
+{
+	uint rng_state = seed;
+    rng_state ^= (rng_state << 13);
+    rng_state ^= (rng_state >> 17);
+    rng_state ^= (rng_state << 5);
+    return float( rng_state ) * (1.0 / 4294967296.0);
+}
 
 vec4 randColor( uint seed )
 {
-	float tr = (seed%16)/16.0f;
-	float tg = (seed%8)/8.0f;
-	float tb = (seed%4)/4.0f;
+	float tr = rand(wang_hash(seed+1));
+	float tg = rand(wang_hash(seed+2));
+	float tb = rand(wang_hash(seed+3));
 	return vec4( 
 					0.3 + 0.4 * tr, 
 					0.3 + 0.4 * tg, 
@@ -39,17 +58,17 @@ void main()
 	vec4 glowColor = texture2D(glowMap, uvFS);
 
 	// interpolate skin color from light to dark
-	float factor = (rnd_seed%16)/16.0f;
-	vec4 colorR = mix(vec4( 1.0f, 0.89, 0.78, 1.0f ), vec4( 0.18, 0.13, 0.12, 1.0f ), factor);  // Dark skin tone
+	float factor = rand(wang_hash(rnd_seed+4));
+	vec4 colorR = mix(vec4( 1.0f, 0.89, 0.78, 1.0f ), vec4( 0.09, 0.07, 0.06, 1.0f ), factor);  // Dark skin tone
 	
 	// Create random colors for shirts
 	vec4 colorG = randColor(rnd_seed); // Shirt
 
 	// Create random colors for pants
-	vec4 colorB = randColor(2*rnd_seed + 55);// 0.5 * (vec4(0.1f, 0.5f, 0.8f, 1.0f) + randColor(rnd_seed+52)); // Pants
+	vec4 colorB = randColor(2*rnd_seed);// 0.5 * (vec4(0.1f, 0.5f, 0.8f, 1.0f) + randColor(rnd_seed+52)); // Pants
 	
 	// Create random colors for Accessories/Extra
-	//vec4 colorA = randColor(rnd_seed + 55); // Extra
+	//vec4 colorA = randColor(4*rnd_seed); // Extra
 	
 	vec4 result = diffuseColor;
 	result = mix(result, diffuseColor*colorR, blendMap.r); // Skin
