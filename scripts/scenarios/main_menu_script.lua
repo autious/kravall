7e2368f87d78130ef0c69d121010e7d6619e6fce
Max Danielsson <maxdata1@gmail.com>
2014-01-31 18:00:43 +0100

    Adds functional buttons in main and binds them to callbacks.

--- scripts/scenarios/main_menu_script.lua
+++ scripts/scenarios/main_menu_script.lua
@@ -1,29 +1,70 @@
 local input = require "input" 
+local MainMenu = require "gui/MainMenu"
+local SetupMenu = require "gui/SetupMenu"
+local ScenarioMenu = require "gui/ScenarioMenu"
 
 local MenuScrollSpeed = 500
 
 return function( scen )
+    local menuState = {}
+
+    function menuState.goMain()
+        scen.gamemode.camera:setGoal( scen.cameras.main.view, MenuScrollSpeed )
+
+        if scen.gui ~= nil then
+            scen.gui:destroy()
+            scen.gui = nil
+        end
+        scen.gui = MainMenu:new({},menuState)
+    end
+
+    function menuState.goScenario()
+        scen.gamemode.camera:setGoal( scen.cameras.scen.view, MenuScrollSpeed )
+
+        if scen.gui ~= nil then
+            scen.gui:destroy()
+            scen.gui = nil
+        end
+        scen.gui = ScenarioMenu:new({},menuState)
+    end
+
+    function menuState.goSetup()
+        scen.gamemode.camera:setGoal( scen.cameras.setup.view, MenuScrollSpeed )
+
+        if scen.gui ~= nil then
+            scen.gui:destroy()
+            scen.gui = nil
+        end
+        scen.gui = SetupMenu:new({},menuState)
+    end
+
     local function onKey( key, scancode, action )
         if key == core.input.keyboard.key.A and action == core.input.action.Release then
-            scen.gamemode.camera:setGoal( scen.cameras.main.view, MenuScrollSpeed )
+            menuState.goMain()
         end  
 
         if key == core.input.keyboard.key.B and action == core.input.action.Release then
-            scen.gamemode.camera:setGoal( scen.cameras.scen.view, MenuScrollSpeed )
+            menuState.goScenario()
         end  
 
         if key == core.input.keyboard.key.C and action == core.input.action.Release then
-            scen.gamemode.camera:setGoal( scen.cameras.setup.view, MenuScrollSpeed )
+            menuState.goSetup()
         end  
     end
 
     local function init()
         scen.gamemode.camera:setView( scen.cameras.main.view ) 
         input.registerOnKey( onKey )
+        menuState.goMain()
     end
 
     local function destroy()
         input.deregisterOnKey( onKey )
+        
+        if scen.gui ~= nil then
+            scen.gui:destroy()
+            scen.gui = nil
+        end
     end
 
     scen:registerInitCallback( init ) 
