b3d9f8e8bc7fddf55107f0eb6fb5d5ffafdb0055
Max Danielsson <maxdata1@gmail.com>
2014-01-31 16:52:23 +0100

    Adds a main menu where a and b moves between cameras.

--- scripts/scenarios/main_menu_script.lua
+++ scripts/scenarios/main_menu_script.lua
@@ -1,11 +1,30 @@
-return function( scen )
-    scen:registerInitCallback( function()
-        print( "Starting main menu" )       
-    end) 
+local input = require "input" 
 
-    scen.cameras = scen.cameras or {}
-    scen.cameras.first = { {}, {} }
+return function( scen )
+
+    local function onKey( key, scancode, action )
+        if key == core.input.keyboard.key.A and action == core.input.action.Release then
+            scen.gamemode.camera:setGoal( scen.cameras.main.view )
+        end  
+
+        if key == core.input.keyboard.key.B and action == core.input.action.Release then
+            scen.gamemode.camera:setGoal( scen.cameras.setup.view )
+        end  
+    end
+
     
+
+    local function init()
+        input.registerOnKey( onKey )
+    end
+
+    local function destroy()
+        input.deregisterOnKey( onKey )
+    end
+
+    scen:registerInitCallback( init ) 
+    scen:registerDestroyCallback( destroy )
+
     scen.gamemode = require "gamemodes/menu":new()
     scen:registerUpdateCallback( function(delta) scen.gamemode:update(delta) end )
     scen:registerDestroyCallback( function() scen.gamemode:destroy() end )
