bc6984841c43eadfe996318c26314a24ccf7270d
alexander vestman <alexander.vestman@gmail.com>
2014-02-06 10:51:38 +0100

    Adds the ability to cancel unit formation placement with left clicking while positioning

--- scripts/gamemodes/kravall.lua
+++ scripts/gamemodes/kravall.lua
@@ -16,6 +16,10 @@ core.movementData.setMovementMetaData( core.movementData.Sprinting, 8.8, 17, 14 
 
 function squadHandling()
     if core.input.mouse.isButtonDownOnce(core.input.mouse.button.Left) then        
+        if isClick == true then
+            isClick = false
+        end
+
         selectedEntity = core.system.picking.getLastHitEntity()
         if selectedEntity then
             local unitTypeComponent = selectedEntity:get(core.componentType.UnitTypeComponent);
@@ -45,24 +49,26 @@ function squadHandling()
         end        
     elseif core.input.mouse.isButtonDownOnce(core.input.mouse.button.Right) then
         if #selectedSquads > 0 then
+            isClick = true
             local mouseX, mouseY = core.input.mouse.getPosition()
             clickStartX, clickStartY, clickStartZ = core.system.picking.getGroundHit(mouseX, mouseY);
         end   
     elseif core.input.mouse.isButtonDown(core.input.mouse.button.Right) then        
-        if #selectedSquads > 0 and clickStartX and clickStartY and clickStartZ then
+        if #selectedSquads > 0 and clickStartX and clickStartY and clickStartZ and isClick then
             local mouseX, mouseY = core.input.mouse.getPosition()
             local dragX, dragY, dragZ = core.system.picking.getGroundHit(mouseX, mouseY)    
 
             core.system.squad.previewSquadFormation(selectedSquads, core.system.squad.formations.LineFormation, clickStartX, clickStartY, clickStartZ, dragX, dragY, dragZ)
         end
     elseif core.input.mouse.isButtonUp(core.input.mouse.button.Right) then
-        if #selectedSquads > 0 and clickStartX and clickStartY and clickStartZ then
+        if #selectedSquads > 0 and clickStartX and clickStartY and clickStartZ and isClick then
             local mouseX, mouseY = core.input.mouse.getPosition()
             clickEndX, clickEndY, clickEndZ = core.system.picking.getGroundHit(mouseX, mouseY)    
             core.system.squad.setSquadFormation(selectedSquads, core.system.squad.formations.LineFormation, clickStartX, clickStartY, clickStartZ, clickEndX, clickEndY, clickEndZ)
 	        core.system.squad.setSquadGoal(selectedSquads, clickStartX, clickStartY, clickStartZ)
 
             clickStartX, clickStartY, clickStartZ = nil, nil, nil
+            isClick = false
         end
     end
 
@@ -70,8 +76,14 @@ function squadHandling()
         if #selectedSquads > 0 then
             core.system.squad.setSquadStance(selectedSquads, core.PoliceStance.Aggressive)
         end   
-    else
-        core.system.squad.setSquadStance(selectedSquads, core.PoliceStance.Passive)
+    elseif core.input.keyboard.isKeyDown(core.input.keyboard.key.O) then
+        if #selectedSquads > 0 then
+            core.system.squad.setSquadStance(selectedSquads, core.PoliceStance.Defensive)
+        end   
+    elseif core.input.keyboard.isKeyDown(core.input.keyboard.key.P) then
+        if #selectedSquads > 0 then
+            core.system.squad.setSquadStance(selectedSquads, core.PoliceStance.Passive)
+        end
     end
 end
 
