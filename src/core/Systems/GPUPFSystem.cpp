79cbf6d6f7b7de1bba2e9fea303748333468d793
Tim Henriksson <henriksson.tim@gmail.com>
2014-02-06 15:52:51 +0100

    fixes

--- src/core/Systems/GPUPFSystem.cpp
+++ src/core/Systems/GPUPFSystem.cpp
@@ -46,98 +46,95 @@ namespace Core
 		}
 		else
 		{
-			if (m_entities.size() > 0)
+			glUseProgram(m_shaderID);
+
+			//if (!m_readBack)
 			{
-				glUseProgram(m_shaderID);
-
-				//if (!m_readBack)
+				//Set data in shader
+				glBindBuffer(GL_SHADER_STORAGE_BUFFER, m_inDataBuffer);
+			
+				DataIN* in = (DataIN*)glMapBufferRange(GL_SHADER_STORAGE_BUFFER, 0, MAXIMUM_ENTITIES * sizeof(DataIN),
+					GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT | GL_MAP_UNSYNCHRONIZED_BIT);
+				int i = 0;
+				for (std::vector<Entity>::iterator it = m_entities.begin(); it != m_entities.end(); it++)
 				{
-					//Set data in shader
-					glBindBuffer(GL_SHADER_STORAGE_BUFFER, m_inDataBuffer);
-
-					DataIN* in = (DataIN*)glMapBufferRange(GL_SHADER_STORAGE_BUFFER, 0, m_entities.size() * sizeof(DataIN),
-						GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT | GL_MAP_UNSYNCHRONIZED_BIT);
-					int i = 0;
-					for (std::vector<Entity>::iterator it = m_entities.begin(); it != m_entities.end(); it++)
-					{
-						WorldPositionComponent* wpc = WGETC<WorldPositionComponent>(*it);
-						MovementComponent* mc = WGETC<MovementComponent>(*it);
-						UnitTypeComponent* utc = WGETC<UnitTypeComponent>(*it);
-						AttributeComponent* ac = WGETC<AttributeComponent>(*it);
-
-						in[i].position_unitType = glm::vec4(wpc->position[0], wpc->position[1], wpc->position[2], utc->type);
-						in[i].newDirection_empty = glm::vec4(mc->newDirection[0], mc->newDirection[1], mc->newDirection[2], mc->speed);
-
-						if (utc->type == UnitType::Rioter)
-						{
-							in[i].health_stamina_morale_stancealignment = glm::vec4(ac->health, ac->stamina, ac->morale, ac->rioter.stance);
-							in[i].groupSquadID_defenseRage_mobilityPressure_empty = glm::vec4(ac->rioter.groupID, ac->rioter.rage, ac->rioter.pressure, 0);
-						}
-						else if (utc->type == UnitType::Police)
-						{
-							in[i].health_stamina_morale_stancealignment = glm::vec4(ac->health, ac->stamina, ac->morale, ac->rioter.stance); //Attacking state should override stance here
-							in[i].groupSquadID_defenseRage_mobilityPressure_empty = glm::vec4(ac->rioter.groupID, 1, 1, 0);
-						}
-
-						i++;
-					}
-					glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
-					glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
-					glInvalidateBufferData(m_inDataBuffer);
-
-					//Set entity count
-					glUniform1ui(m_entityCount, i);
-
-					//Run the shader
-					glDispatchCompute(GLuint(1), GLuint(1), GLuint(1));
-					//}
-					//else
-					//{
-					glMemoryBarrier(GL_ALL_BARRIER_BITS);
-
-					//Get data from shader
+					WorldPositionComponent* wpc = WGETC<WorldPositionComponent>(*it);
+					MovementComponent* mc = WGETC<MovementComponent>(*it);
+					UnitTypeComponent* utc = WGETC<UnitTypeComponent>(*it);
+					AttributeComponent* ac = WGETC<AttributeComponent>(*it);
+			
+					in[i].position_unitType = glm::vec4(wpc->position[0], wpc->position[1], wpc->position[2], utc->type);
+					in[i].newDirection_empty = glm::vec4(mc->newDirection[0], mc->newDirection[1], mc->newDirection[2], mc->speed);
 					
-					glBindBuffer(GL_SHADER_STORAGE_BUFFER, m_outDataBuffer);
-					DataOUT* out = (DataOUT*)glMapBufferRange(GL_SHADER_STORAGE_BUFFER, 0, m_entities.size() * sizeof(DataOUT), GL_MAP_READ_BIT);
-
-					i = 0;
-					for (std::vector<Entity>::iterator it = m_entities.begin(); it != m_entities.end(); it++)
+					if (utc->type == UnitType::Rioter)
 					{
-						MovementComponent* mc = WGETC<MovementComponent>(*it);
-						AttributeComponent* ac = WGETC<AttributeComponent>(*it);
-						UnitTypeComponent* utc = WGETC<UnitTypeComponent>(*it);
-
-						mc->newDirection[0] = out[i].newDirection_speed.x;
-						mc->newDirection[1] = out[i].newDirection_speed.y;
-						mc->newDirection[2] = out[i].newDirection_speed.z;
-						mc->speed = out[i].newDirection_speed.w;
-
-						ac->morale = out[i].morale_rage_pressure_empty.x;
-
-						if (utc->type == UnitType::Rioter)
-						{
-							ac->rioter.rage = out[i].morale_rage_pressure_empty.y;
-							ac->rioter.pressure = out[i].morale_rage_pressure_empty.z;
-
-							ac->rioter.rage = glm::clamp(ac->rioter.rage, 0.0f, 100.0f);
-							ac->rioter.pressure = glm::clamp(ac->rioter.pressure, 0.0f, 100.0f);
-						}
-
-						ac->morale = glm::clamp(ac->morale, 0.0f, 2.0f);
-
-						i++;
+						in[i].health_stamina_morale_stancealignment = glm::vec4(ac->health, ac->stamina, ac->morale, ac->rioter.stance);
+						in[i].groupSquadID_defenseRage_mobilityPressure_empty = glm::vec4(ac->rioter.groupID, ac->rioter.rage, ac->rioter.pressure, 0);
 					}
-					glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
-					glInvalidateBufferData(m_outDataBuffer);
-					glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
-
+					else if (utc->type == UnitType::Police)
+					{
+						in[i].health_stamina_morale_stancealignment = glm::vec4(ac->health, ac->stamina, ac->morale, ac->rioter.stance); //Attacking state should override stance here
+						in[i].groupSquadID_defenseRage_mobilityPressure_empty = glm::vec4(ac->rioter.groupID, 1, 1, 0);
+					}
+						
+					i++;
 				}
+				glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
+				glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+				glInvalidateBufferData(m_inDataBuffer);
 
-				m_readBack = !m_readBack;
-
-				//Reset program
-				glUseProgram(0);
+				//Set entity count
+				glUniform1ui(m_entityCount, i);
+		
+				//Run the shader
+				glDispatchCompute(GLuint(1), GLuint(1), GLuint(1));
 			}
+			//else
+			{
+				glMemoryBarrier(GL_ALL_BARRIER_BITS);
+			
+				//Get data from shader
+				glBindBuffer(GL_SHADER_STORAGE_BUFFER, m_outDataBuffer);
+				DataOUT* out = (DataOUT*)glMapBufferRange(GL_SHADER_STORAGE_BUFFER, 0, MAXIMUM_ENTITIES * sizeof(DataOUT), GL_MAP_READ_BIT);
+
+				int i = 0;
+				for (std::vector<Entity>::iterator it = m_entities.begin(); it != m_entities.end(); it++)
+				{
+					MovementComponent* mc = WGETC<MovementComponent>(*it);
+					AttributeComponent* ac = WGETC<AttributeComponent>(*it);
+					UnitTypeComponent* utc = WGETC<UnitTypeComponent>(*it);
+
+					mc->newDirection[0] = out[i].newDirection_speed.x;
+					mc->newDirection[1] = out[i].newDirection_speed.y;
+					mc->newDirection[2] = out[i].newDirection_speed.z;
+					mc->speed = out[i].newDirection_speed.w;
+
+					ac->morale = out[i].morale_rage_pressure_empty.x;
+					
+					if (utc->type == UnitType::Rioter)
+					{
+						ac->rioter.rage = out[i].morale_rage_pressure_empty.y;
+						ac->rioter.pressure = out[i].morale_rage_pressure_empty.z;
+
+						ac->rioter.rage = glm::clamp(ac->rioter.rage, 0.0f, 100.0f);
+						ac->rioter.pressure = glm::clamp(ac->rioter.pressure, 0.0f, 100.0f);
+					}
+
+					ac->morale = glm::clamp(ac->morale, 0.0f, 2.0f);
+					
+					i++;
+				}
+				glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
+				glInvalidateBufferData(m_outDataBuffer);
+				glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+			
+			}
+			
+			m_readBack = !m_readBack;
+			
+			//Reset program
+			glUseProgram(0);
+
 		}
 
 	}
