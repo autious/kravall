5f0f4acea9b7e57bac03471cdec52382c8538d54
Max Danielsson <maxdata1@gmail.com>
2014-02-07 13:25:13 +0100

    First functional ability to select and start scenario.

--- scripts/gui/SetupMenu.lua
+++ scripts/gui/SetupMenu.lua
@@ -3,16 +3,28 @@ local Button = require "gui/component/Button"
 local Slider = require "gui/component/Slider"
 local Checkbox = require "gui/component/Checkbox"
 local CenterPlacer = require "gui/placement/CenterPlacer"
+local TextLabel = require "gui/component/TextLabel"
 
 SetupMenu = {}
 
-function SetupMenu:new(o)
+function SetupMenu:new(o,menuState)
     o = o or {}
 
     o.gui = GUI:new()
 
-    o.gui:addComponent(Button:new({xoffset=10,yoffset=10}))
-    o.gui:addComponent(Button:new({xoffset=10,yoffset=10}))
+    local scenarioNameLabel = TextLabel:new({ xoffset=0, yoffset=0, anchor="NorthEast", label="Undefined scenario"})
+
+    if menuState.selectedScenario ~= nil then
+        scenarioNameLabel:setLabel( menuState.selectedScenario.name )    
+    end
+
+    o.gui:addComponent(scenarioNameLabel)
+    o.gui:addComponent(Button:new({
+
+                                    matReleased = "assets/texture/ui/go-button-release.material",
+                                    matPressed = "assets/texture/ui/go-button-press.material",
+                                    matHover = "assets/texture/ui/go-button-hover.material",
+                                    anchor="SouthEast",xoffset=0,yoffset=0,onClick = function() openscenario( menuState.selectedScenario.filename ) end }))
 
     o.gui:addPlacementHandler( CenterPlacer:new() )
 
