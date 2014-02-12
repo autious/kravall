a04374b55a9f0b55a3f74449f0f724bbe022bd9e
Tim Henriksson <henriksson.tim@gmail.com>
2014-02-12 13:49:35 +0100

    shaders

--- shaders/decals/DecalsFS.glsl
+++ shaders/decals/DecalsFS.glsl
@@ -11,6 +11,8 @@ uniform float gGamma;
 uniform mat4 invProjView;
 uniform mat4 invModelMatrix;
 
+
+
 vec4 reconstruct_pos(float z, vec2 uv_f)
 {
     vec4 sPos = vec4(uv_f * 2.0 - 1.0, z, 1.0);
@@ -22,18 +24,16 @@ layout ( location = 1 ) out vec4 diffuseRT;
 layout ( location = 2 ) out vec4 specularRT;
 layout ( location = 3 ) out vec4 glowMatIDRT;
 
-out vec4 resultColor;
-
 void main()
 {
 	vec4 normal_depth = texture2D(gNormalDepth, uvFS);
 	vec4 gBufferPos = reconstruct_pos(normal_depth.w, posFS.xy);
 
 	//Transform the gBufferPos into decals local space
-	//vec3 decalPos = invModelMatrix * gBufferPos
+	vec4 decalPos = invModelMatrix * gBufferPos;
 
 	//use the xy position of the position for the texture lookup
-	//vec2 texcoord = decalPos.xy * recipDecalSize * 0.5 + 0.5
-	//gl_FragColor = texture2D(diffuseDecalTexture, texcoord)
+	vec2 texcoord = decalPos.xy * 1.0f / textureSize(gDiffuse, 0) * 0.5 + 0.5;
+	diffuseRT = texture2D(gDiffuse, texcoord);
 
 }
