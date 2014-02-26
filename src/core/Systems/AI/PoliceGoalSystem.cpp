66521300138a54e5db005510ec7051196d1096c0
John Heiding <John_Heiding@hotmail.com>
2014-02-26 16:41:53 +0100

    fix on threadpool. no longer borked

--- src/core/Systems/AI/PoliceGoalSystem.cpp
+++ src/core/Systems/AI/PoliceGoalSystem.cpp
@@ -55,10 +55,7 @@ void Core::PoliceGoalSystem::Update( float delta )
 		policeList[ head++ ] = *it;
 	}
 
-	//int nrCores = Core::world.m_threadHandler.GetNrThreads();
-	//int nrPerCore = std::ceil((float)m_entities.size() / (float)nrCores);
-
-	int nrCores = 1;//Core::world.m_threadHandler.GetNrThreads();
+	int nrCores = Core::world.m_threadHandler.GetNrThreads();
 	int nrPerCore = std::ceil((float)head / (float)nrCores);
 
 	for( int i = 0; i < nrCores; i++ )
