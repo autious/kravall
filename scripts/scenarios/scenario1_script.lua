a5a838a4d8416fd9e4c922d0b0150df19c091976
Max Danielsson <maxdata1@gmail.com>
2014-02-28 09:52:52 +0100

    Adds rotateYLight

--- scripts/scenarios/scenario1_script.lua
+++ scripts/scenarios/scenario1_script.lua
@@ -427,5 +427,21 @@ return function( scen )
         ent:set( core.componentType.RotationComponent, { rotation = {qrot:get()}})
     end
 
+
+    function T.rotateYLight( ent, delta )
+
+        local r = ent:get(core.componentType.RotationComponent).rotation
+        r[4] = 0
+        local vec = core.glm.vec4.new(unpack(r))
+
+        local qrot = core.glm.quat.new( )
+        
+        qrot = qrot:rotate( delta*10, core.glm.vec3.new(0,1,0) )
+        local d = qrot:mat4Cast() 
+        local newD = d * vec     
+        
+        ent:set( core.componentType.RotationComponent, { rotation = {newD:get()}})
+    end
+
     return T
 end
