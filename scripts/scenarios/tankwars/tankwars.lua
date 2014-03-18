9b727f5ecc7800d5c8a12d4a936ae1195fd8bc8b
Johan Anderdahl <nukeson@live.se>
2014-03-18 22:26:19 +0100

    Fixed tankwars

--- scripts/scenarios/tankwars/tankwars.lua
+++ scripts/scenarios/tankwars/tankwars.lua
@@ -119,22 +119,22 @@ local function CreateEnemyTankBody(position, group)
 		type = core.componentType.WorldPositionComponent,
 		data = { position = { position[1], 0, position[2] } }
 	},
-	--{
-	--	type = core.componentType.UnitTypeComponent,
-	--	data = { unitType = core.UnitType.Rioter }
-	--},
-	--{
-	--	type = core.componentType.MovementComponent,
-	--	data = { direction = { 0, 0, 0 }, newDirection = { 0, 0, 0 }, speed = 5.0, 
-	--	desiredSpeed = { 5.0, 5.0, 5.0, 5.0 }, goal = false }
-	--	,ignoreHard = true
-	--},
-	--{
-	--	type = core.componentType.BoundingVolumeComponent,
-	--	data = { sphereOffset = { 0, 0, 0 }, sphereRadius = 3.0, pickingRadius = 0.0,
-	--			collisionModel = core.BoundingVolumeCollisionModel.DynamicResolution, 
-	--			type = core.BoundingVolumeType.SphereBoundingType }
-	--},
+	{
+		type = core.componentType.UnitTypeComponent,
+		data = { unitType = core.UnitType.Rioter }
+	},
+	{
+		type = core.componentType.MovementComponent,
+		data = { direction = { 0, 0, 0 }, newDirection = { 0, 0, 0 }, speed = 5.0, 
+		desiredSpeed = { 5.0, 5.0, 5.0, 5.0 }, goal = false }
+		,ignoreHard = true
+	},
+	{
+		type = core.componentType.BoundingVolumeComponent,
+		data = { sphereOffset = { 0, 0, 0 }, sphereRadius = 4.0, pickingRadius = 0.0,
+				collisionModel = core.BoundingVolumeCollisionModel.DynamicResolution, 
+				type = core.BoundingVolumeType.SphereBoundingType }
+	},
 	{
 		type = core.componentType.GraphicsComponent,
 		data = { mesh = 0, material = 0, type = core.gfx.objectTypes.OpaqueGeometry, layer = core.gfx.layerTypes.MeshLayer, outlineColor = {0, 0, 1, 1}, render = true },
@@ -143,29 +143,34 @@ local function CreateEnemyTankBody(position, group)
 					material = { core.loaders.MaterialLoader, "assets/texture/tankwars/leopard-body.material", false }
 			   }
 	},
-	--{
-	--	type = core.componentType.AttributeComponent,
-	--	data = { health = 100, stamina = 100, morale = 2.0, 
-	--		   alignment = core.RioterAlignment.Anarchist, rage = 0, pressure = 0, groupID = group, stanceRioter = core.RioterStance.Normal}
-	--	,
-	--	ignoreHard = true
-	--},
-	--{
-	--	type = core.componentType.TargetingComponent,
-	--	data = { weapon = -1 },
-	--	ignoreHard = true
-	--},
+	{
+		type = core.componentType.AttributeComponent,
+		data = { health = 100, stamina = 100, morale = 2.0, 
+			   alignment = core.RioterAlignment.Anarchist, rage = 0, pressure = 0, groupID = group, stanceRioter = core.RioterStance.Normal}
+		,
+		ignoreHard = true
+	},
+	{
+		type = core.componentType.TargetingComponent,
+		data = { weapon = -1 },
+		ignoreHard = true
+	},
 	{
 		type = core.componentType.ScaleComponent,
 		data = { scale = 1.0 }
 	},
-	--{
-	--	type = core.componentType.FlowfieldComponent,
-	--	data = { node = -1 }
-	--},
+	{
+		type = core.componentType.FlowfieldComponent,
+		data = { node = -1 }
+	},
 	{
 		type = core.componentType.RotationComponent,
 		data = { rotation = { 0, 0, 0, 1 } }
+	},
+	{
+		type = core.componentType.MovementDataComponent,
+		data = { prevDt = 1.0 },
+		ignoreHard = true
 	}
 })
 end
@@ -242,7 +247,7 @@ for i=0,n do
 end
 
 
-local nnn = 30
+local nnn = 15
 for i=0,nnn do
 	for j=0,nnn do
 		
@@ -251,7 +256,7 @@ for i=0,nnn do
 		{
 			{
 				type = core.componentType.WorldPositionComponent,
-				data = { position = { (i - nnn / 2) * 15 + math.random() * 10, 0, (j - nnn / 2) * 15 + math.random() * 10} }
+				data = { position = { (i - nnn / 2) * 30 + math.random() * 20, 0, (j - nnn / 2) * 30 + math.random() * 20} }
 			},
 			{
 				type = core.componentType.GraphicsComponent,
@@ -268,6 +273,23 @@ for i=0,nnn do
 			{
 				type = core.componentType.RotationComponent,
 				data = { rotation = { 0, math.sin(tmpRot), 0, math.cos(tmpRot) } }
+			},
+			{
+				type = core.componentType.BoundingVolumeComponent,
+				data = { sphereOffset = { 0, 0, 0 }, sphereRadius = 3.0, pickingRadius = 0.0,
+						collisionModel = core.BoundingVolumeCollisionModel.StaticResolution, 
+						type = core.BoundingVolumeType.SphereBoundingType }
+			},
+			{
+				type = core.componentType.MovementComponent,
+				data = { direction = { 0, 0, 0 }, newDirection = { 0, 0, 0 }, speed = 0, 
+				desiredSpeed = 0, goal = false }
+				,ignoreHard = true
+			},
+			{
+				type = core.componentType.MovementDataComponent,
+				data = { prevDt = 1.0 },
+				ignoreHard = true
 			}
 		}
 		)
@@ -349,43 +371,48 @@ local playerTank = scen:loadAssembly(
 		type = core.componentType.ScaleComponent,
 		data = { scale = 1.0 }
 	},
-	--{
-	--	type = core.componentType.UnitTypeComponent,
-	--	data = { unitType = core.UnitType.Rioter }
-	--},
+	{
+		type = core.componentType.UnitTypeComponent,
+		data = { unitType = core.UnitType.Rioter }
+	},
 	{
 		type = core.componentType.MovementComponent,
 		data = { direction = { 0, 0, 0 }, newDirection = { 0, 0, 0 }, speed = 1.5, 
 		desiredSpeed = 1.5, goal = false }
 		,ignoreHard = true
 	},
-	--{
-	--	type = core.componentType.BoundingVolumeComponent,
-	--	data = { sphereOffset = { 0, 0, 0 }, sphereRadius = 5.0, pickingRadius = 0.0, 
-	--			collisionModel = core.BoundingVolumeCollisionModel.DynamicResolution, 
-	--			type = core.BoundingVolumeType.SphereBoundingType }
-	--},
-	--{
-	--	type = core.componentType.AttributeComponent,
-	--	data = { health = 100, stamina = 100, morale = 2.0, 
-	--		   alignment = core.RioterAlignment.Anarchist, rage = 0, pressure = 0, groupID = playerGroup, stanceRioter = core.RioterStance.Normal}
-	--	,
-	--	ignoreHard = true
-	--},
+	{
+		type = core.componentType.BoundingVolumeComponent,
+		data = { sphereOffset = { 0, 0, 0 }, sphereRadius = 4.0, pickingRadius = 0.0, 
+				collisionModel = core.BoundingVolumeCollisionModel.DynamicResolution, 
+				type = core.BoundingVolumeType.SphereBoundingType }
+	},
+	{
+		type = core.componentType.AttributeComponent,
+		data = { health = 100, stamina = 100, morale = 2.0, 
+			   alignment = core.RioterAlignment.Anarchist, rage = 0, pressure = 0, groupID = playerGroup, stanceRioter = core.RioterStance.Normal}
+		,
+		ignoreHard = true
+	},
 	{
 		type = core.componentType.RotationComponent,
 		--data = { rotation = { 0, 1*math.sin( -3.14/4 ), 0, math.cos( -3.14/4 ) } }
 		data = { rotation = { 0, 0, 0, 1 } }
 	},
-	--{
-	--	type = core.componentType.FlowfieldComponent,
-	--	data = { node = -1 }
-	--},
-	--{
-	--	type = core.componentType.TargetingComponent,
-	--	data = { weapon = -1 },
-	--	ignoreHard = true
-	--}
+	{
+		type = core.componentType.FlowfieldComponent,
+		data = { node = -1 }
+	},
+	{
+		type = core.componentType.TargetingComponent,
+		data = { weapon = -1 },
+		ignoreHard = true
+	},
+	{
+		type = core.componentType.MovementDataComponent,
+		data = { prevDt = 1.0 },
+		ignoreHard = true
+	}
 }
 )
 
@@ -767,7 +794,7 @@ local enemyTankTurrets = {}
 local tankGroups
 
 local function InitEnemyTanks(n)
-	--tankGroups = tankGroups or core.system.groups.createGroup();
+	tankGroups = tankGroups or core.system.groups.createGroup();
 	local minSpawn = 20
 	local maxSpawn = (boundX + boundY) / 2 - minSpawn
 	for i=1,n do
@@ -1205,7 +1232,7 @@ end
 local function UpdateEnemyTanks(delta)
 	local playerPos = playerTank:get(core.componentType.WorldPositionComponent).position
 	
-	--core.system.groups.setGroupGoal(tankGroups, playerPos[1], playerPos[2], playerPos[3] )
+	core.system.groups.setGroupGoal(tankGroups, playerPos[1], playerPos[2], playerPos[3] )
 	
 	local i = 1
 	while i <= #enemyTanks do
